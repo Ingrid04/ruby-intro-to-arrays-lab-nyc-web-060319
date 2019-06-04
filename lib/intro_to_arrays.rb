d8211e986ae91642c183a14ceeea33d2ae17df6a
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-04 14:36:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,10 +13,10 @@ def array_with_two_elements()
 
 end
 
-def first_element()
+def first_element(taylor_swift)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   first_element =   @taylor_swift.first
-    return first_element
+    return first_element(taylor_swift)
 end
   
 
