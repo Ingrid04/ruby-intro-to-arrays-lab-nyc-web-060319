947cf922f16886927a2c8577a23ce3ba9464405a
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-06-04 14:24:37 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,57 @@
+
+def instantiate_new_array()
+   @my_new_array = Array.new
+   @my_new_array.size
+      return @my_new_array
+end
+
+def array_with_two_elements()
+   
+   @my_two_array = Array.new(2)
+   @my_two_array.size
+      return @my_two_array
+
+end
+
+def first_element
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift[0]
+    return @taylor_swift[0]
+end
+  
+
+def my_third_element
+   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+     return my_third_element[2]
+end
+
+def my_last_element
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift[3]
+     return my_last_element[3]
+end
+
+def first_element_with_array_methods
+  @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_country = @south_east_asia[1]
+   return first_country
+end
+
+def last_element_with_array_methods
+  @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  last_country = @south_east_asia[3]
+    return last_country
+end
+
+def length_of_array
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  @programming_languages.length
+end
+  
+
+
+
+
+
+
+
